10c91b58819f953e88918d6e073f1c2a41b165cd
moonshine17 <furaokolo@gmail.com>
2023-10-13 23:04:02 +0300

    1

---
+++ NewPlayer/UserControls/PopularSong.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace NewPlayer.UserControls
+{
+    public partial class PopularSong : UserControl
+    {
+        public PopularSong()
+        {
+            InitializeComponent();
+        }
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register
+            ("Title", typeof(string), typeof(PopularSong));
+
+        public string Time
+        {
+            get { return (string)GetValue(TimeProperty); }
+            set { SetValue(TimeProperty, value); }
+        }
+
+        public static readonly DependencyProperty TimeProperty = DependencyProperty.Register
+            ("Title", typeof(string), typeof(PopularSong));
+
+        public bool IsActive
+        {
+            get { return (bool)GetValue(IsActiveProperty); }
+            set { SetValue(IsActiveProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsActiveProperty = DependencyProperty.Register
+            ("Title", typeof(bool), typeof(PopularSong));
+
+        public ImageSource Image
+        {
+            get { return (ImageSource)GetValue(ImageProperty); }
+            set { SetValue(ImageProperty, value); }
+        }
+
+        public static readonly DependencyProperty ImageProperty = DependencyProperty.Register
+            ("Title", typeof(ImageSource), typeof(PopularSong));
+
+
+
+
+
+
+
+
+    }
+}
